f50b537c36d67c4fb428cdac187353229ea4c94a
stevenacz <scoailazaa@gmail.com>
2020-11-09 16:35:50 -0500

    Problema 13

--- 13my_all/my_all.rb
+++ 13my_all/my_all.rb
@@ -1,3 +1,13 @@
 def my_all?(array, &block)
-  # your code here
+  result = true
+  if block_given?
+    array.each { |item| !block.call(item) && result = false }
+  else
+    array.each { |item| !item && result = false }
+  end
+  result
 end
+
+numbers = [1, 2, 3, 5]
+new_arr = my_all?(numbers) { |number| number.is_a?(Integer) } # => true
+p my_all?(numbers) { |number| number < 4 } # => false
