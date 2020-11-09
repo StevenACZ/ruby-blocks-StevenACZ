f6173fc5cc912faa1964534c9803e60447324e81
stevenacz <scoailazaa@gmail.com>
2020-11-09 16:40:31 -0500

    Problema 14

--- 14my_any/my_any.rb
+++ 14my_any/my_any.rb
@@ -1,3 +1,9 @@
 def my_any?(array, &block)
-  # your code here
+  result = false
+  if block_given?
+    array.each { |item| block.call(item) && result = true }
+  else
+    array.each { |item| item && result = true }
+  end
+  result
 end
