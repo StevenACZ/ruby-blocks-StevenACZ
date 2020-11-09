94eab9753587880a60382490454eb9203f573e33
stevenacz <scoailazaa@gmail.com>
2020-11-09 15:48:38 -0500

    Problema 01

--- 12my_map/my_map.rb
+++ 12my_map/my_map.rb
@@ -1,3 +1,15 @@
 def my_map(array, &block)
-  # your code here
+  if block_given?
+    new_arr = []
+    array.each do |item|
+      new_arr.push(yield item.to_i) 
+    end
+    new_arr
+  else
+    array.to_enum
+  end
 end
+
+string_array = ["1", "2", "3", "4"]
+double_int_array = my_map(string_array) { |string_num| string_num.to_i * 2 }
+p double_int_array
