3f8de8a14aa668502098c2f14138048b1b5001aa
stevenacz <scoailazaa@gmail.com>
2020-11-09 15:52:38 -0500

    Problema 12

--- 12my_map/my_map.rb
+++ 12my_map/my_map.rb
@@ -2,7 +2,7 @@ def my_map(array, &block)
   if block_given?
     new_arr = []
     array.each do |item|
-      new_arr.push(yield item.to_i) 
+      new_arr.push(block.call(item.to_i))
     end
     new_arr
   else
@@ -10,6 +10,6 @@ def my_map(array, &block)
   end
 end
 
-string_array = ["1", "2", "3", "4"]
-double_int_array = my_map(string_array) { |string_num| string_num.to_i * 2 }
-p double_int_array
+# string_array = ["1", "2", "3", "4"]
+# double_int_array = my_map(string_array) { |string_num| string_num.to_i * 2 }
+# p double_int_array
