d4a75ae279764ad236d303c6a61635ff419a2082
stevenacz <scoailazaa@gmail.com>
2020-11-09 17:00:47 -0500

    Problema 15

--- 15my_select/my_select.rb
+++ 15my_select/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(array)
-  # your code here
+def my_select(array, &block)
+  new_arr = []
+  if block_given?
+    array.each { |item| block.call(item) && new_arr.push(item) }
+    new_arr
+  else
+    array.to_enum
+  end
 end
