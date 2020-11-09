faf68f25e8c50883c214a31b7c04476af02ddca5
stevenacz <scoailazaa@gmail.com>
2020-11-09 17:23:46 -0500

    Problema 16

--- 16my_find/my_find.rb
+++ 16my_find/my_find.rb
@@ -1,3 +1,10 @@
-def my_find(array)
-  # your code here
+def my_find(array, &block)
+  result = nil
+  array.each do |item|
+    if block_given? ? block.call(item) : item
+      result = item
+      break
+    end
+  end
+  block_given? ? result : array.to_enum
 end
